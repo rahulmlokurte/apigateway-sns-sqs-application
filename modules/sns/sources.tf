ab07ba860e10d8c8a834bf7cc1458342d2cbb435
rahulmlokurte <rahul.m.lokurte@gmail.com>
2022-02-09 00:24:21 +0530

    fix the template location

--- modules/sns/sources.tf
+++ modules/sns/sources.tf
@@ -1,5 +1,5 @@
 data "template_file" "workflow_sns" {
-  template = file("${path.module}/templates/workfall_sns_policy.json")
+  template = file("${path.module}/template/workfall_sns_policy.json")
   vars = {
     workfall_sns_arn = aws_sns_topic.workfall_sns.arn
   }
