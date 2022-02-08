70a43ee16f476022af1c4bf086b046085412cc7e
rahulmlokurte <rahul.m.lokurte@gmail.com>
2022-02-08 20:51:16 +0530

    fix apigateway logs

--- modules/api_gateways/workfall-apigateway.tf
+++ modules/api_gateways/workfall-apigateway.tf
@@ -9,28 +9,28 @@ resource "aws_api_gateway_resource" "workfall_api" {
 }
 
 resource "aws_api_gateway_method" "workfall_api" {
-  authorization = "NONE"
-  http_method   = "GET"
-  resource_id   = aws_api_gateway_resource.workfall_api.id
-  rest_api_id   = aws_api_gateway_rest_api.workfall_api.id
+  authorization      = "NONE"
+  http_method        = "GET"
+  resource_id        = aws_api_gateway_resource.workfall_api.id
+  rest_api_id        = aws_api_gateway_rest_api.workfall_api.id
   request_parameters = {
     "method.request.querystring.TopicArn" = false
-    "method.request.querystring.Message" = false
+    "method.request.querystring.Message"  = false
   }
 }
 
 resource "aws_api_gateway_integration" "workfall_api" {
-  http_method = aws_api_gateway_method.workfall_api.http_method
-  resource_id = aws_api_gateway_resource.workfall_api.id
-  rest_api_id = aws_api_gateway_rest_api.workfall_api.id
-  type        = "AWS"
+  http_method             = aws_api_gateway_method.workfall_api.http_method
+  resource_id             = aws_api_gateway_resource.workfall_api.id
+  rest_api_id             = aws_api_gateway_rest_api.workfall_api.id
+  type                    = "AWS"
   integration_http_method = "POST"
-  uri = "arn:aws:apigateway:${var.aws_region}:sns:action/Publish"
-  request_parameters = {
+  uri                     = "arn:aws:apigateway:${var.aws_region}:sns:action/Publish"
+  request_parameters      = {
     "integration.request.querystring.TopicArn" = "method.request.querystring.TopicArn"
-    "integration.request.querystring.Message" = "method.request.querystring.Message"
+    "integration.request.querystring.Message"  = "method.request.querystring.Message"
   }
-  credentials = var.api_gateway_role_arn
+  credentials             = var.api_gateway_role_arn
 }
 
 resource "aws_api_gateway_deployment" "workfall_api" {
@@ -44,11 +44,11 @@ resource "aws_api_gateway_stage" "workfall_api" {
   deployment_id = aws_api_gateway_deployment.workfall_api.id
   rest_api_id   = aws_api_gateway_rest_api.workfall_api.id
   stage_name    = "workfall"
-  depends_on = [aws_cloudwatch_log_group.workfall_api]
 }
 
 resource "aws_cloudwatch_log_group" "workfall_api" {
-  name = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.workfall_api.id}/${aws_api_gateway_stage.workfall_api.stage_name}"
+  name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.workfall_api.id}/${aws_api_gateway_stage.workfall_api.stage_name}"
   retention_in_days = 7
+  depends_on = [aws_api_gateway_deployment.workfall_api]
 }
 
